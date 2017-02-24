ad1add2b5c2384731e3fafa288d9fca83182e963
Caleb Saunders <csaunders1@toromail.csudh.edu>
2017-02-24 15:29:18 -0800

    Fixed csv again with right price

--- handlers.go
+++ handlers.go
@@ -40,7 +40,7 @@ func GetCSVWebOrders(w http.ResponseWriter, r *http.Request) {
 	writer := csv.NewWriter(b)
 	writer.Write([]string{"URL", "Decision", "Price", "Notes", "Category"})
 	for _, order := range orders {
-		writer.Write([]string{order.URL, order.Decision, order.PriceStr, order.Notes})
+		writer.Write([]string{order.URL, order.Decision, string(order.PriceStr), order.Notes})
 	}
 	writer.Flush()
 
